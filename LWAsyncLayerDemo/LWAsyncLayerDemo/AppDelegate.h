69ded257a87408f1dbbb4e519a45a3b0f9fa4de0
waynezxcv <liuweiself@126.com>
2016-01-21 14:59:49 +0800

    first commit

---
+++ LWAsyncLayerDemo/LWAsyncLayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWAsyncLayerDemo
+//
+//  Created by 刘微 on 16/1/21.
+//  Copyright © 2016年 Warm+. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LWAsyncLayerDemo/LWAsyncLayerDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LWAsyncLayerDemo
+//
+//  Created by 刘微 on 16/1/21.
+//  Copyright © 2016年 Warm+. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
