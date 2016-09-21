36345a4ad0b1ad684db4ed1aa5c9c25bfc7d2567
Wei Liu <liuweiself@126.com>
2016-09-22 03:28:10 +0800

    update

--- Gallop/Utils/GallopDefine.h
+++ Gallop/Utils/GallopDefine.h
@@ -9,12 +9,21 @@
 #ifndef GallopDefine_h
 #define GallopDefine_h
 
+#ifndef SCREEN_WIDTH
 #define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#endif
+
+#ifndef SCREEN_HEIGHT
 #define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#endif  
+
+#ifndef SCREEN_BOUNDS
 #define SCREEN_BOUNDS [UIScreen mainScreen].bounds
+#endif
+
+#ifndef RGB
 #define RGB(A,B,C,D) [UIColor colorWithRed:A/255.0f green:B/255.0f blue:C/255.0f alpha:D]
-
-
+#endif
 
 
 #endif /* GallopDefine_h */
